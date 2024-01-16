c2d59ccde26dce8cb4dddd331e8b8753e5f18ad1
herber30 <herbert.oliveira07@outlook.com>
2024-01-15 21:14:25 -0300

    adição de pagina Js (Alerta ao clicar no Botão de enviar mensagem)

--- js/forum.js
+++ js/forum.js
@@ -1,5 +1,5 @@
 const submitButton = document.querySelector("input[type='submit']");
         submitButton.addEventListener("click", () => {
-          alert("NÃO FOI POSSÍVEL ENVIAR A MENSAGEM! Tópico encerrado.");
+          alert("NÃO FOI POSSÍVEL ENVIAR A MENSAGEM! Chat encerrado.");
           submitButton.preventDefault();
         });
