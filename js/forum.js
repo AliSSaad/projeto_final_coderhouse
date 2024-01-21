3edf262ebbf25b9d0dba8622f61409145ea423a9
herber30 <herbert.oliveira07@outlook.com>
2024-01-21 11:44:01 -0300

    Atualização e correção de alertas

--- js/forum.js
+++ js/forum.js
@@ -1,5 +1,16 @@
+// Alerta de chat encerrado ao tentar enviar mensagem
 const submitButton = document.querySelector("input[type='submit']");
 submitButton.addEventListener("click", (event) => {
   alert("NÃO FOI POSSÍVEL ENVIAR A MENSAGEM! Chat encerrado.");
   event.preventDefault();
+});
+
+// Alerta ao tentar criar um topico novo
+const form = document.querySelector(".forum__topico");
+form.addEventListener("submit", (e) => {
+  const titulo = document.querySelector("#titulo").value;
+  if (titulo === "") {
+    alert("O título do tópico é obrigatório.");
+    e.preventDefault();
+  }
 });
