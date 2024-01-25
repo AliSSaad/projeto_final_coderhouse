bc58912b91dcadadd2784e46d8f3717fc7e835f0
Ali Saad <alisaads321@gmail.com>
2024-01-24 21:50:59 -0300

    apiEpisodios

--- js/apiEpisodios.js
+++ js/apiEpisodios.js
@@ -1,11 +1,17 @@
 function armazenarNome(data, a){
     let numeroEpisodio = a;
     let titulo = document.getElementById(`titulo-${numeroEpisodio}`);
+    console.log(titulo.id)
     let nomeEpisodio = data;
-    titulo.innerHTML = `Episodio ${numeroEpisodio} - ${nomeEpisodio}`;
+    if (numeroEpisodio >=12){
+        titulo.innerHTML = `Episodio ${numeroEpisodio - 11} - ${nomeEpisodio}`;
+    }
+    else if (numeroEpisodio < 12){
+        titulo.innerHTML = `Episodio ${numeroEpisodio} - ${nomeEpisodio}`;
+    }
 }
 
-for(let a = 1; a < 15; a++){
+for(let a = 1; a < 21; a++){
     fetch("https://rickandmortyapi.com/api/episode")
     .then(response => response.json())
     .then(data => armazenarNome(data[`results`][a - 1]['name'], a));
